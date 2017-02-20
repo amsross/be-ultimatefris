ed5d86c63c6c87c937518b468a4583e6609f4d8f
Matt Ross <amsross@gmail.com>
2017-02-20 16:21:21 -0500

    Test api reducers and effects

---
+++ models/api.js
@@ -0,0 +1,43 @@
+const r = require("ramda");
+const uuid = require("uuid/v4");
+const storage = require("../lib/storage");
+const store = r.tryCatch(
+  x => x(window.localStorage),
+  e => storage({
+    "games": [],
+  }))(storage);
+
+module.exports = {
+  state: {
+    "games": [],
+    "game": null,
+  },
+  reducers: {
+    receiveGames: (state, games) => {
+      return r.assoc("games", games, state);
+    },
+    selectGame: (state, game) => {
+      return r.compose(
+        r.converge(r.assoc("game"), [
+          r.compose(
+            r.either(r.find(r.propEq("id", game.id)), r.always(game)),
+            r.prop("games")),
+          r.identity,
+        ]))(state);
+    },
+  },
+  effects: {
+    createGame: (state, data, send, done) => {
+      store.create("games", game => send("selectGame", game, done));
+    },
+    readGames: (state, data, send, done) => {
+      store.read("games", games => send("receiveGames", games, done));
+    },
+    updateGame: (state, data, send, done) => {
+      store.update("games", data, game => send("receiveGames", game, done));
+    },
+    deleteGame: (state, data, send, done) => {
+      store.delete("games", data, game => send("receiveGames", game, done));
+    },
+  },
+};
---
+++ test/models/api.js
@@ -0,0 +1,126 @@
+const sinon = require("sinon");
+const test = require("tape");
+const api = require("../../models/api");
+
+test("models/api", assert => {
+
+  assert.ok(api.state, "property exists");
+
+  test("reducers", assert => {
+
+    assert.ok(api.reducers, "property exists");
+
+    assert.test("receiveGames", assert => {
+      const actual = api.reducers.receiveGames({
+        "foo": "bar",
+        "games": [],
+      }, [{
+        "bar": "foo"
+      }]);
+      const expected = {
+        "foo": "bar",
+        "games": [{
+          "bar": "foo"
+        }]
+      };
+
+      assert.deepEqual(actual, expected, "should update 'games' prop without mutating other props");
+
+      assert.end();
+    });
+
+    assert.test("selectGame", assert => {
+      const actual = api.reducers.selectGame({
+        "games": [{"id": 1}, {"id": 2}],
+      }, {"id": 2});
+      const expected = {
+        "game": {"id": 2},
+        "games": [{"id": 1}, {"id": 2}],
+      }
+
+      assert.deepEqual(actual, expected, "should set the passed game on the state's 'game' prop");
+
+      assert.end();
+    });
+
+    assert.end();
+  });
+
+  test("effects", assert => {
+
+    assert.ok(api.effects, "property exists");
+
+    assert.test("createGame", assert => {
+      assert.ok(api.effects.createGame, "method exists");
+
+      const send = sinon.spy((storeName, data, done) => done());
+
+      api.effects.createGame({}, {}, send, () => {
+        assert.ok(send.calledOnce, "send was called once");
+        assert.equal(send.args[0][0], "selectGame", "results sent to correct reducer");
+
+        assert.equal(typeof send.args[0][1], "object", "object passed as new game");
+        assert.equal(typeof send.args[0][1].id, "string", "new game has an id");
+
+        assert.end();
+      });
+    });
+
+    assert.test("readGames", assert => {
+      assert.ok(api.effects.readGames, "method exists");
+
+      const send = sinon.spy((storeName, data, done) => done());
+
+      api.effects.readGames({}, {}, send, () => {
+        assert.ok(send.calledOnce, "send was called once");
+        assert.equal(send.args[0][0], "receiveGames", "results sent to correct reducer");
+
+        assert.ok(send.args[0][1] instanceof Array, "returns array");
+
+        assert.end();
+      });
+    });
+
+    assert.test("updateGame", assert => {
+      assert.ok(api.effects.updateGame, "method exists");
+
+      const send = sinon.spy((storeName, data, done) => done());
+
+      api.effects.updateGame({}, {"id": 1}, send, () => {
+        assert.ok(send.calledOnce, "send was called once");
+        assert.equal(send.args[0][0], "receiveGames", "results sent to correct reducer");
+
+        assert.ok(send.args[0][1] instanceof Array, "returns array");
+        assert.equal(send.args[0][1][0].id, 1, "updated game is returned in state");
+
+        assert.end();
+      });
+    });
+
+    assert.test("deleteGame", assert => {
+      assert.ok(api.effects.deleteGame, "method exists");
+
+      const send = sinon.spy((storeName, data, done) => done());
+
+      api.effects.updateGame({}, {"id": 1}, send, () => {
+        assert.ok(send.calledOnce, "send was called once");
+        assert.equal(send.args[0][1].length, 1, "a game was returned");
+
+        api.effects.deleteGame({}, {"id": 1}, send, () => {
+          assert.ok(send.calledTwice, "send was called a second time");
+
+          assert.equal(send.args[1][0], "receiveGames", "results sent to correct reducer");
+          assert.ok(send.args[1][1] instanceof Array, "returns array");
+          assert.equal(send.args[1][1].length, 0, "only game was deleted");
+
+          assert.end();
+        });
+      });
+    });
+
+    assert.end();
+  });
+
+  assert.end();
+});
+
