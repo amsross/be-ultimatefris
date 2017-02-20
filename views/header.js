992b22e91b31049aecf9e0e7b51362a3849ce5a6
Matt Ross <amsross@gmail.com>
2017-02-20 16:55:28 -0500

    Test header view

---
+++ views/header.js
@@ -0,0 +1,11 @@
+const html = require("choo/html");
+
+module.exports = function view(state, prev, send) {
+  return html`
+    <nav class="w-100 border-box pa3 ph5 bg-light-gray">
+      <div class="v-mid w-100 tr">
+        <a class="f6 link dim br3 ph3 pv2 mb2 dib ba bw1 dark-green" title="Create" onclick=${() => send("createGame")}>Create</a>
+      </div>
+    </nav>
+    `;
+};
