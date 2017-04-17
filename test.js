96dbc43a8564920caabb827862a159c9b689dc8b
Matt Ross <amsross@gmail.com>
2017-04-17 06:53:34 -0400

    Remove currently untestable views' tests

--- test.js
+++ test.js
@@ -4,11 +4,6 @@ const browserify = require('browserify')
 
 browserify([
   `${__dirname}/test/models/api.js`
-  // `${__dirname}/test/pages/home.js`,
-  // `${__dirname}/test/views/game.js`,
-  // `${__dirname}/test/views/games-list.js`,
-  // `${__dirname}/test/views/header.js`,
-  // `${__dirname}/test/views/map.js`
 ])
   .bundle()
   .pipe(run())
