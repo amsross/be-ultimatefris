84cf139633ab20a1af1ad737a57d7eb6c0298bc9
Matt Ross <amsross@gmail.com>
2017-04-17 07:03:32 -0400

    Add main entrypoint

---
+++ client.js
@@ -0,0 +1,15 @@
+const sheetify = require('sheetify')
+const choo = require('choo')
+const logger = require('choo-log')
+const Home = require('./pages/home')
+const app = choo()
+
+app.use(logger())
+app.use(require('./models/api'))
+
+sheetify('./public/css/leaflet.css')
+sheetify('tachyons')
+app.route('/', Home)
+
+const tree = app.start()
+document.body.appendChild(tree)
